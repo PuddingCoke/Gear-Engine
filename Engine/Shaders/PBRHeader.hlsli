96df316aff2ccfe17bc9d6eb27e64835dbec35a4
PuddingCoke <zhangxianzhe1231@outlook.com>
2026-05-11 23:54:52 +0800

    学习了Moving Frostbite to Physically Based Rendering 3.0中一些改良公式，并把用于解析光源的BRDF计算导出到了引擎项目中的PBRHeader.hlsli

---
+++ Engine/Shaders/PBRHeader.hlsli
@@ -0,0 +1,80 @@
+﻿float GGXDistribution(float3 M, float3 N, float roughness)
+{
+    //alphaG = r^2 书中的公式
+    float alphaG2 = roughness * roughness;
+    
+    alphaG2 *= alphaG2;
+    
+    float MdotN = saturate(dot(M, N));
+    
+    float denominator = MdotN * MdotN * (alphaG2 - 1.0) + 1.0;
+    
+    denominator = denominator * denominator;
+    
+    return alphaG2 / denominator;
+}
+
+//这个函数使用了优化算法，其结果已经除以了4*NdotL*NdotV
+float HeightCorrelatedSmithG_2(float NdotL, float NdotV, float roughness)
+{
+    float alphaG = roughness * roughness;
+    
+    float alphaG2 = alphaG * alphaG;
+    
+    float Lambda_GGXV = NdotL * sqrt((-NdotV * alphaG2 + NdotV) * NdotV + alphaG2);
+    
+    float Lambda_GGXL = NdotV * sqrt((-NdotL * alphaG2 + NdotL) * NdotL + alphaG2);
+    
+    return 0.5f / (Lambda_GGXV + Lambda_GGXL);
+}
+
+float3 FresnelSchlick(float cosTheta, float3 F0, float3 F90)
+{
+    return F0 + (F90 - F0) * pow(saturate(1.0 - cosTheta), 5.0);
+}
+
+float Fr_DisneyDiffuse(float NdotV, float NdotL, float LdotH, float roughness)
+{
+    float energyBias = lerp(0, 0.5, roughness);
+
+    float energyFactor = lerp(1.0, 1.0 / 1.51, roughness);
+
+    float3 fd90 = (energyBias + 2.0 * LdotH * LdotH * roughness).rrr;
+
+    float3 f0 = float3(1.0f, 1.0, 1.0);
+
+    float lightScatter = FresnelSchlick(NdotL, f0, fd90).r;
+
+    float viewScatter = FresnelSchlick(NdotV, f0, fd90).r;
+
+    return lightScatter * viewScatter * energyFactor;
+}
+
+//用于解析光源
+//输入的所有向量必须为归一化的向量
+//F0和Albedo为使用金属度参数化插值后的结果
+//roughness为用户控制的感知线性粗糙度
+float3 PBR_BRDFEvaluate(float3 N, float3 V, float3 L, float3 F0, float3 Albedo, float roughness)
+{
+    const float3 H = normalize(L + V);
+    
+    const float NDF = GGXDistribution(H, N, roughness);
+    
+    const float3 F = FresnelSchlick(saturate(dot(H, V)), F0, float3(1.0, 1.0, 1.0));
+    
+    const float NdotV = saturate(dot(N, V));
+    
+    const float NdotL = saturate(dot(N, L));
+    
+    const float LdotH = saturate(dot(L, H));
+    
+    const float G = HeightCorrelatedSmithG_2(NdotL, NdotV, roughness);
+    
+    const float3 specular = F * NDF * G;
+    
+    const float3 diffuse = Fr_DisneyDiffuse(NdotV, NdotL, LdotH, roughness) * Albedo;
+    
+    const float PI = 3.14159265358979323846;
+    
+    return (specular + diffuse) / PI;
+}
--- PBRRendering/PBRPS.hlsl
+++ PBRRendering/PBRPS.hlsl
@@ -1,56 +1,6 @@
 ﻿#include"Common.hlsli"
 
-#define PI 3.14159265358979323846
-
-float GGXDistribution(float3 M, float3 N, float roughness)
-{
-    float alphaSqr = roughness * roughness;
-    
-    alphaSqr *= alphaSqr;
-    
-    float MdotN = max(dot(M, N), 0.0);
-    
-    float denominator = MdotN * MdotN * (alphaSqr - 1.0) + 1.0;
-    
-    denominator = PI * denominator * denominator;
-    
-    return alphaSqr / denominator;
-}
-
-float SmithG_1(float NdotV, float k)
-{
-    return NdotV / lerp(NdotV, 1.0, k);
-}
-
-float SmithG_2(float3 N, float3 L, float3 V, float k)
-{
-    const float NdotL = max(dot(N, L), 0.0);
-    
-    const float NdotV = max(dot(N, V), 0.0);
-    
-    return SmithG_1(NdotL, k) * SmithG_1(NdotV, k);
-}
-
-float3 FresnelSchlick(float cosTheta, float3 F0)
-{
-    return F0 + (1 - F0) * pow(saturate(1.0 - cosTheta), 5.0);
-}
-
-//解析光源
-float3 SpecularBRDF(float3 H, float3 N, float3 V, float3 L, float3 F0, float roughness)
-{
-    const float NdotV = max(dot(N, V), 0.0);
-    
-    const float NdotL = max(dot(N, L), 0.0);
-    
-    const float k = (roughness + 1) * (roughness + 1) / 8.0;
-    
-    const float3 numerator = GGXDistribution(H, N, roughness) * FresnelSchlick(saturate(dot(H, V)), F0) * SmithG_2(N, L, V, k);
-    
-    const float denominator = 4.0 * NdotL * NdotV + 0.00001;
-    
-    return numerator / denominator;
-}
+#include"PBRHeader.hlsli"
 
 struct PixelInput
 {
@@ -84,6 +34,8 @@ static Texture2D<float2> brdfLUTTexture = ResourceDescriptorHeap[brdfLUTTextureI
 
 static TextureCube irradianceMap = ResourceDescriptorHeap[irradianceTextureIndex];
 
+#define PI 3.141592653589793
+
 float4 main(PixelInput input) : SV_TARGET
 {
     float3 linearColor = pow(diffuseColor.rgb, 2.2);
@@ -93,8 +45,6 @@ float4 main(PixelInput input) : SV_TARGET
     float3 L = normalize(lightPos.xyz - input.pos.xyz);
     
     float3 V = normalize(perframeResource.eyePos.xyz - input.pos.xyz);
-    
-    float3 H = normalize(L + V);
     
     float3 F0 = float3(0.04, 0.04, 0.04);
     
@@ -106,7 +56,7 @@ float4 main(PixelInput input) : SV_TARGET
     
     float NdotL = saturate(dot(N, L));
     
-    float3 color = ((float3(1.0, 1.0, 1.0) - FresnelSchlick(saturate(dot(H, V)), F0)) * albedo / PI + SpecularBRDF(H, N, V, L, F0, roughness)) * radiance * NdotL;
+    float3 color = PBR_BRDFEvaluate(N, V, L, F0, albedo, roughness) * radiance * NdotL;
     
     if (enableSpecularIBL)
     {
