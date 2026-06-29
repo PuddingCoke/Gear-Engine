fe4af12b79bb5487c3ad9a08945d98446b796c18
PuddingCoke <zhangxianzhe1231@outlook.com>
2026-06-29 21:09:58 +0800

    TODO.bat脚本现在会检测目标位置是否存在TODOAPP.exe，如不存在则会以TODOAPP项目的Release配置先编译项目

--- TODO.bat
+++ TODO.bat
@@ -1,2 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "EXE_PATH=Build\TODOAPP\TODOAPP.exe"
+
+if exist "%EXE_PATH%" goto :run
+
+echo [INFO] TODOAPP.exe not found, building with Release configuration...
+
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+for /f "delims=" %%i in ('call "%VSWHERE%" -latest -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe') do set "MSBUILD=%%i"
+
+if not defined MSBUILD (
+    echo [ERROR] MSBuild not found!
+    pause
+    exit /b 1
+)
+
+"%MSBUILD%" "Gear Engine.sln" /p:Configuration=Release /p:Platform=x64 /t:TODOAPP /v:minimal
+
+if errorlevel 1 (
+    echo [ERROR] Build failed!
+    pause
+    exit /b 1
+)
+
+echo [INFO] Build succeeded.
+
+:run
 cd TODOAPP
 start "" "../Build/TODOAPP/TODOAPP.exe"
