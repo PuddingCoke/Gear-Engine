7c7fdbb9f3758d081f740e039236b847d30b93d1
PuddingCoke <zhangxianzhe1231@outlook.com>
2026-07-01 00:44:00 +0800

    更新了Logger的说明

--- Engine/Inc/Gear/Utils/Logger.h
+++ Engine/Inc/Gear/Utils/Logger.h
@@ -16,13 +16,12 @@ using LogColor = Gear::Utils::Logger::LogColor;
 /// <summary>
 /// 一个可以输出不同颜色的日志记录器
 /// int32_t、uint32_t、float_t这类数值类型会有特殊的颜色
-/// 因为LogContext目前的实现，窄字符串是不被支持的，在编译时会报错
-/// 此外最好不要在循环中使用日志记录器
+/// 最好不要在循环中使用日志记录器
 /// 
 /// 以下是支持的数据类型
 /// 整形：int32_t、int64_t、uint32_t、uint64_t
 /// 浮点：float_t、double_t
-/// 字符串：字面宽字符串、const wchar_t*、std::wstring
+/// 字符串：字面宽字符串、const wchar_t*、std::wstring、字面窄字符串、const char*、std::string
 /// 
 /// 你可以使用IntegerType来指定整数的输出进制，下方为示例代码
 /// LOGUSER(L"32位无符号整数测试", IntegerMode::HEX, 13689u, UINT_MAX, IntegerMode::DEC, 13689u, UINT_MAX);
@@ -30,7 +29,7 @@ using LogColor = Gear::Utils::Logger::LogColor;
 /// 你可以使用FloatPrecision来调整小数点后位数，下方为示例代码
 /// such as LOGUSER(L"浮点测试", FloatPrecision(4), 125.6f, FloatPrecision(2), 125.7);
 /// 
-/// 在LogColor这个类中有很多可用的颜色
+/// LogColor这个类中有很多可用的颜色
 /// </summary>
 namespace Gear::Utils::Logger
 {
